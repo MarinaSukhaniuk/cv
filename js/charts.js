32133c534714511d1f72228d1b5e72df5043ea27
Marina <marinas0595@gmail.com>
2017-04-19 21:34:41 +0300

    first commit

---
+++ js/charts.js
@@ -0,0 +1,22 @@
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Top U.S Smartphone Operating Systems By Market Share, Q3 2012"
+      },
+      data: [
+      {
+       type: "doughnut",
+       dataPoints: [
+       {  y: 53.37, indexLabel: "Android" },
+       {  y: 35.0, indexLabel: "Apple iOS" },
+       {  y: 7, indexLabel: "Blackberry" },
+       {  y: 2, indexLabel: "Windows Phone" },
+       {  y: 5, indexLabel: "Others" }
+       ]
+     }
+     ]
+   });
+
+    chart.render();
+  }
